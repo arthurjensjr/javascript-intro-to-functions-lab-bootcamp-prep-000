b3f362455a5e207fb5680c9c061acfeb9fbaa57c
aj.artwil <aj.artwil@gmail.com>
2019-05-09 18:48:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-function shout(string) {
-  return string.toUpperCase()
-}
-
-function whisper(string) {
-  return string.toLowerCase()
-}
-
-function logshout(string) {
-  console.log.toUpperCasel()
+function shout(string){
+  uppercase = string.toUpperCase();
+  string = uppercase;
+  return uppercase;
 }
