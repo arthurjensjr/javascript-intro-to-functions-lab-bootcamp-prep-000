9c7d87de06511df6927e970b597bee598fe1a42d
aj.artwil <aj.artwil@gmail.com>
2019-05-09 20:07:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,13 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  return 'I can\'t hear you!'; 
-  
-
-  
+     if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+    }
+    if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    }
 }
